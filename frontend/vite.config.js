a7a3fea11496d3694f891b8895efede26624c3d4
ADE <a.delabays@rafisa.ch>
2026-06-22 11:12:42 +0200

    Change compose to differenciate between dev and prod build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,18 @@ import vueDevTools from "vite-plugin-vue-devtools";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    host: "0.0.0.0",
     port: 5174,
+    watch: {
+      usePolling: process.env.VITE_USE_POLLING === "true",
+    },
+    proxy: {
+      "/api": {
+        target: process.env.DEV_API_PROXY_TARGET ?? "http://localhost:8080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
   plugins: [vue(), vueDevTools()],
   resolve: {
